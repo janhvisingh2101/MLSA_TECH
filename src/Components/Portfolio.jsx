0ce88ce381785702c2562fb0ee78083784c54bfa
Janhvisingh21 <janhvisinghshrinet@gmail.com>
2023-09-20 01:10:19 +0530

    add changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/ocean-3605547_1280.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "ocean";
 
 /**
  * Project list
@@ -31,10 +31,10 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Personal Website",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "My Medium Account",
+    url: "https://medium.com/@janhvi2101",
   },
   {
     title: "Web Development for Beginners",
